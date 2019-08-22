ddf0c387c7aba651253b0bf8b3f78cb2f8eb5c83
abhi <abhishek.bhardwaj_cs18@gla.ac.in>
2019-08-22 20:58:20 +0530

    create object for input

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -8,7 +8,6 @@ public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
-        System.out.println("The speed of the object at " + time + " seconds after its release is " + speed + " and the distance the object has travelled in the " + time + " seconds after the relase is " + distance);
 
 		/* TODO 2:
 		 *   a. Declare all your necessary variables inside main
